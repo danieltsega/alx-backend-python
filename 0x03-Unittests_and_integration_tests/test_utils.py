6e081808ef225665dd7c087857a7edadccb654cc
danny <danny@DannyX.myguest.virtualbox.org>
2023-10-30 21:25:06 +0300

    [Feature]: Add test util module

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""
+Test Util Modules
+"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch, Mock
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """_summary_
+
+    Args:
+        unittest (_type_): _description_
+    """
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """_summary_
+        """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_output)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError)
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_output):
+        """_summary_
+        """
+        with self.assertRaises(expected_output) as context:
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """_summary_
+
+    Args:
+                    unittest (_type_): _description_
+    """
+    @parameterized.expand(
+        [
+            ('http://example.com', {'payload': True}),
+            ('http://holberton.io', {'payload': False})
+        ]
+    )
+    def test_get_json(self, url, expected_output):
+        """_summary_
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = expected_output
+        with patch('requests.get', return_value=mock_response):
+            response = get_json(url)
+
+            self.assertEqual(response, expected_output)
+
+
+class TestMemoize(unittest.TestCase):
+    """_summary_
+
+    Args:
+                    unittest (_type_): _description_
+    """
+
+    def test_memoize(self):
+        """_summary_
+
+        Returns:
+                _type_: _description_
+        """
+
+        class TestClass:
+            """_summary_
+            """
+
+            def a_method(self):
+                """_summary_
+
+                Returns:
+                        _type_: _description_
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """_summary_
+
+                Returns:
+                        _type_: _description_
+                """
+                return self.a_method()
+
+        test_obj = TestClass()
+
+        with patch.object(test_obj, 'a_method') as mock_method:
+            mock_method.return_value = 42
+
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
